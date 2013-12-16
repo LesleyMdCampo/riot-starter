51b61757e7ffa3798365f1e10c7f9ce67a9adc2e
LesleyMdCampo <lesliz32@gmail.com>
2013-12-15 18:54:22 -0600

    Add loop function to main.js

--- js/main.js
+++ js/main.js
@@ -5,5 +5,30 @@ var videos = [
 ];
 
 var renderVideoList = function () {
-  // TODO
+  for (var i=0; i < videos.length; i++) {
+		var title = items[i]['title'];
+		console.log(title);
+		var list = $.render(itemTemplate, { title: title } );
+		$('#video-list').append(list);
+	}
 };
+
+
+// var itemTemplate = $('#templates .item').html();
+
+// var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+// $('#store').append(newItemHtml);
+
+// var items = [
+//   { name: 'Mayer', price: 1 },
+//   { name: 'Natalie', price: 2 },
+//   { name: 'Lesley', price: 3 }
+// ];
+
+// for (var i=0; i < items.length; i++) {
+// 	var bob = items[i]['name'];
+// 	var smith = items[i]['price'];
+// 	// console.log(name + ": $" + price);
+// 	var pleaseWork = $.render(itemTemplate,  { name: bob, price: smith} );
+// 	$('#store').append(pleaseWork);
+// }
