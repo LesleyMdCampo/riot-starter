e97f3d601f6ac86a9b8def71bf61563e922a6ee7
LesleyMdCampo <lesliz32@gmail.com>
2013-12-15 19:12:23 -0600

    Edit loop function in main.js to work.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 
+var itemTemplate = $('#templates .item').html();
+
 var videos = [
   { title: 'Get Krazy', youtubeId: 'GyR2HJ9B7aM' },
   { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
 ];
 
 var renderVideoList = function () {
-  for (var i=0; i < videos.length; i++) {
-		var title = items[i]['title'];
+  for (var i = 0; i < videos.length; i += 1) {
+		var title = videos[i]['title'];
 		console.log(title);
-		var list = $.render(itemTemplate, { title: title } );
-		$('#video-list').append(list);
 	}
+	var list = $.render(itemTemplate, { title: title } );
+		$('#video-list').append(list);
 };
 
+renderVideoList();
 
-// var itemTemplate = $('#templates .item').html();
 
-// var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
-// $('#store').append(newItemHtml);
 
 // var items = [
 //   { name: 'Mayer', price: 1 },
@@ -32,3 +32,4 @@ var renderVideoList = function () {
 // 	var pleaseWork = $.render(itemTemplate,  { name: bob, price: smith} );
 // 	$('#store').append(pleaseWork);
 // }
+
